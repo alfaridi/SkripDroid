3c22f0f32a8ba8414445f9205720169070579b33
alfaridi <dichi.alfaridi@gmail.com>
2011-06-08 17:47:21 +0700

    Make some skeleton.

--- src/info/alfaridi/skripdroid/SkripDroidActivity.java
+++ src/info/alfaridi/skripdroid/SkripDroidActivity.java
@@ -1,13 +1,69 @@
 package info.alfaridi.skripdroid;
 
-import android.app.Activity;
-import android.os.Bundle;
+import greendroid.app.GDActivity;
+import greendroid.graphics.drawable.ActionBarDrawable;
+import greendroid.widget.ActionBarItem;
+import greendroid.widget.NormalActionBarItem;
+import info.alfaridi.skripdroid.utils.Base64;
 
-public class SkripDroidActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.pm.Signature;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class SkripDroidActivity extends GDActivity {
+	/** Called when the activity is first created. */
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setActionBarContentView(R.layout.main);
+
+		addActionBarItem(
+				getActionBar().newActionBarItem(NormalActionBarItem.class)
+						.setDrawable(
+								new ActionBarDrawable(getResources(),
+										R.drawable.ic_action_bar_info)),
+				R.id.action_bar_view_info);
+
+		try {
+			PackageInfo info = getPackageManager().getPackageInfo(
+					"GDSkripsi.apk", PackageManager.GET_SIGNATURES);
+			for (Signature signature : info.signatures) {
+				MessageDigest md = MessageDigest.getInstance("SHA");
+				md.update(signature.toByteArray());
+				Log.i("PXR", Base64.encodeBytes(md.digest()));
+			}
+		} catch (NameNotFoundException e) {
+		} catch (NoSuchAlgorithmException e) {
+		}
+	}
+
+	@Override
+	public boolean onHandleActionBarItemClick(ActionBarItem item, int position) {
+		switch (item.getItemId()) {
+		case R.id.action_bar_view_info:
+			startActivity(new Intent(this, InfoTabActivity.class));
+			return true;
+
+		default:
+			return super.onHandleActionBarItemClick(item, position);
+		}
+	}
+
+	public void onFacebookClick(View view) {
+		startActivity(new Intent(this, FacebookActivity.class));
+	}
+
+	public void onTwitterClick(View view) {
+		startActivity(new Intent(this, TwitterActivity.class));
+	}
+
 }
