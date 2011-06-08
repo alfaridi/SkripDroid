0754078db1624a880301d22bbec6bd5acb3bb142
alfaridi <dichi.alfaridi@gmail.com>
2011-06-08 17:44:46 +0700

    This class reserved for placing your APP_ID form facebook and twitter. You must insert your own APP_ID to make this app work.

---
+++ src/info/alfaridi/skripdroid/utils/Token.java
@@ -0,0 +1,11 @@
+package info.alfaridi.skripdroid.utils;
+
+public class Token {
+	public static final String FACEBOOK_APP_ID = "";
+	public static final String FACEBOOK_API_KEY = "";
+	public static final String FACEBOOK_APP_SECRET = "";
+
+	public static final String TWITTER_API_KEY = "";
+	public static final String TWITTER_CONSUMER_KEY = "";
+	public static final String TWITTER_CONSUMER_SECRET = "";
+}
